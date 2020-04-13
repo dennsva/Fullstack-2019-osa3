e6a7774e09ff2d0145447cee587dd87cec53db0a
Dennis <dennsva@gmail.com>
2020-04-13 22:28:53 +0300

    uibuild

---
+++ puhelinluettelo-backend/build/precache-manifest.c5f144cb524ab93a2fbdc7f312520359.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40e9a4a422d4753cc14c",
+    "url": "/static/css/main.3e4dae66.chunk.css"
+  },
+  {
+    "revision": "40e9a4a422d4753cc14c",
+    "url": "/static/js/main.43576117.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a90870d434b459e6fc3c",
+    "url": "/static/js/2.08be6a14.chunk.js"
+  },
+  {
+    "revision": "7f301863a8c6851f5bcddde8032114dc",
+    "url": "/index.html"
+  }
+];
